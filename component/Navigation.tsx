45f76652eb4b3481bf77656a2ad878f70a58a3a0
jk <jklagrosa.dev@gmail.com>
2022-07-31 18:23:44 +0800

    Added offcanvas for links

--- component/Navigation.tsx
+++ component/Navigation.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 import styles from "../styles/Nav.module.scss";
 import { AbbrTagStyled } from "../styles/utils";
@@ -12,59 +13,85 @@ import { MdMenuOpen } from "react-icons/md";
 import { IconButton } from "@chakra-ui/react";
 
 const Navigation = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Navbar expand="lg" className={styles._navigation_wrapper_}>
-      <Container fluid="lg" className="p-0">
-        <Navbar.Brand className={styles._navigation_brand_logo_}>
-          PELI<span>KULA</span>
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="basic-navbar-nav"
-          style={{ outline: "none", boxShadow: "none", border: "none" }}
-        >
-          <MdMenuOpen className={styles._hamburger_menu_} />
-        </Navbar.Toggle>
-        <Navbar.Collapse
-          id="basic-navbar-nav"
-          className={styles._navigation_collapse_}
-        >
-          <Nav className="me-auto" id={styles._navigation_links_wrapper_}>
-            <Nav.Link href="#link">Home</Nav.Link>
-            <Nav.Link href="#home">Trending</Nav.Link>
-            <Nav.Link href="#link">New</Nav.Link>
-            <Nav.Link href="#link">Popular</Nav.Link>
-            <Nav.Link href="#link">My List</Nav.Link>
-          </Nav>
-          {/*  */}
-          <Nav className="ms-auto" id={styles._navigation_links_wrapper_}>
-            <Nav.Link>
-              <AbbrTagStyled title="Search">
-                <IconButton
-                  aria-label="Search Films"
-                  icon={
-                    <BsSearch className={styles._navigation_links_main_icon_} />
-                  }
-                  className={styles._navigation_links_icon_}
-                />
-              </AbbrTagStyled>
-            </Nav.Link>
-            <Nav.Link>
-              <AbbrTagStyled title="User Settings">
-                <IconButton
-                  aria-label="User Settings"
-                  icon={
-                    <BsPersonFill
-                      className={styles._navigation_links_main_icon_}
-                    />
-                  }
-                  className={styles._navigation_links_icon_}
-                />
-              </AbbrTagStyled>
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <>
+      <Navbar expand="lg" className={styles._navigation_wrapper_}>
+        <Container fluid="lg" className="p-0">
+          <Navbar.Brand className={styles._navigation_brand_logo_}>
+            PELI<span>KULA</span>
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            style={{ outline: "none", boxShadow: "none", border: "none" }}
+          >
+            <MdMenuOpen
+              className={styles._hamburger_menu_}
+              onClick={handleShow}
+            />
+          </Navbar.Toggle>
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className={styles._navigation_collapse_}
+          >
+            <Nav className="me-auto" id={styles._navigation_links_wrapper_}>
+              <Nav.Link href="#link">Home</Nav.Link>
+              <Nav.Link href="#home">Trending</Nav.Link>
+              <Nav.Link href="#link">New</Nav.Link>
+              <Nav.Link href="#link">Popular</Nav.Link>
+              <Nav.Link href="#link">My List</Nav.Link>
+            </Nav>
+            {/*  */}
+            <Nav className="ms-auto" id={styles._navigation_links_wrapper_}>
+              <Nav.Link>
+                <AbbrTagStyled title="Search">
+                  <IconButton
+                    aria-label="Search Films"
+                    icon={
+                      <BsSearch
+                        className={styles._navigation_links_main_icon_}
+                      />
+                    }
+                    className={styles._navigation_links_icon_}
+                  />
+                </AbbrTagStyled>
+              </Nav.Link>
+              <Nav.Link>
+                <AbbrTagStyled title="User Settings">
+                  <IconButton
+                    aria-label="User Settings"
+                    icon={
+                      <BsPersonFill
+                        className={styles._navigation_links_main_icon_}
+                      />
+                    }
+                    className={styles._navigation_links_icon_}
+                  />
+                </AbbrTagStyled>
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      {/* LINKS OFFCANVAS */}
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+      {/* END */}
+
+      {/* ################################################################# */}
+    </>
   );
 };
 
