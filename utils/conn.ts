fe80cf6f95707f0db271a1f2b54764326eeda150
jk <jklagrosa.dev@gmail.com>
2022-08-01 20:08:49 +0800

    Added utils folder

---
+++ utils/conn.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const Dbconnection = async () => {
+  if (mongoose.connections[0].readyState) {
+    console.log("DB already connected");
+    return;
+  }
+
+  return await mongoose
+    .connect(process.env.mongo_uri as string)
+    .then((result) => {
+      if (result) return;
+    })
+    .catch((err) => console.log("DB Error: " + err));
+};
+export default Dbconnection;
