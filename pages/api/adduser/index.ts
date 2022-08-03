87146b4cb8cd57d5f4202e9eaebd0301704e3915
jk <jklagrosa.dev@gmail.com>
2022-08-03 21:05:24 +0800

    Fixed issue at adduser endpoint

--- pages/api/adduser/index.ts
+++ pages/api/adduser/index.ts
@@ -7,23 +7,6 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "GET") {
-    await Conn();
-
-    const GET_ALL_MOVIES = await Movie.find({});
-    if (!GET_ALL_MOVIES) {
-      return res.status(400).json({
-        success: false,
-        data: null,
-      });
-    }
-
-    return res.status(200).json({
-      success: true,
-      data: GET_ALL_MOVIES,
-    });
-  }
-
   if (req.method === "POST") {
     await Conn();
 
