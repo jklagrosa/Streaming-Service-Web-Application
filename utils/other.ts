beebafacbf7b357dfaa68d3db8f60c94e24fb623
jk <jklagrosa.dev@gmail.com>
2022-08-04 20:09:57 +0800

    Deployed web application

--- utils/other.ts
+++ utils/other.ts
@@ -1,6 +1,5 @@
-export const BASE_URL = "http://localhost:3000/";
-
-
+// export const BASE_URL = "http://localhost:3000/";
+export const BASE_URL = "https://streaming-service-web-application.vercel.app/";
 
 export const headersOpts = {
   headers: {
